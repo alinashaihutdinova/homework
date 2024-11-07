8c538d23670644b8ca5e2e06e3d29fd557815de9
Admin <Admin@DESKTOP-5QFVAAD>
2024-11-08 01:08:52 +0300

    Добавьте файлы проекта.

---
+++ задачи Тумакова/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задачи_Тумакова
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("упр 2.1");
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name);
+            */
+
+            /*
+            Console.WriteLine("упр 2.2");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (b == 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                double d = (double)a / b;
+                Console.WriteLine(d);
+            }
+            */
+
+            /*
+            Console.WriteLine("дз 2.1");
+
+
+
+
+
+
+            */
+
+            /*
+            Console.WriteLine("дз 2.2");
+            double a3 = Convert.ToDouble(Console.ReadLine());
+            double b3 = Convert.ToDouble(Console.ReadLine());
+            double c3 = Convert.ToDouble(Console.ReadLine());
+            double D = Math.Pow(b3, 2) - 4 * a3 * c3;
+            if (D < 0)
+            {
+                Console.WriteLine("Нет корней");
+            }
+            else
+            {
+                double корень1 = (-b3 - Math.Sqrt(D)) / (2 * a3);
+                double корень2 = (-b3 + Math.Sqrt(D)) / (2 * a3);
+                Console.WriteLine(корень1 + " " + корень2);
+            }
+            */
+        }
+    }
+}
---
+++ задачи/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+
+namespace задачи
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Console.WriteLine("#1");
+            Console.WriteLine("{0: 0.0}", Math.E);
+            */
+
+
+            /*
+            Console.WriteLine("#2");
+            Console.WriteLine("50");
+            Console.WriteLine("10");
+            */
+
+
+            /*
+            Console.WriteLine("#3");
+            Console.WriteLine("5");
+            Console.WriteLine("9");
+            Console.WriteLine("3");
+            Console.WriteLine("8");
+            */
+
+            /*
+            Console.WriteLine("#4");
+            int a4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a4 + 10);
+            */
+
+
+            /*
+            Console.WriteLine("#5");
+            int a5 = Convert.ToInt32(Console.ReadLine());
+            int P = 4 * a5;
+            Console.WriteLine(P);
+            */
+
+            /*
+            Console.WriteLine("#6");
+            int r = Convert.ToInt32(Console.ReadLine());
+            double l = 3.14 * 2 * r;
+            double C = 3.14 * r * r;
+            Console.WriteLine(l + " " + C);
+            */
+
+            /*
+            Console.Writeline("#7");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Math.Cos(x);
+            Console.WriteLine(y);
+            */
+
+            /*
+            Console.Writeline("#8");
+            double o1 = Convert.ToDouble(Console.ReadLine());
+            double o2 = Convert.ToDouble(Console.ReadLine());
+            double v = Convert.ToDouble(Console.ReadLine());
+            double d = (o2 - o1) / 2;
+            double g = Math.Sqrt((Math.Pow(v,2) + Math.Pow(d,2)));
+            Console.WriteLine(2 * g + o1 + o2);
+            */
+
+            /*
+            Console.WriteLine("#9");
+            double x9 = Convert.ToDouble(Console.ReadLine()); //кг конфет
+            double y9 = Convert.ToDouble(Console.ReadLine()); //кг печенья
+            double z9 = Convert.ToDouble(Console.ReadLine()); //кг яблок
+            
+            double priceS = Convert.ToDouble(Console.ReadLine()); //цена конфет
+            double priceC = Convert.ToDouble(Console.ReadLine()); //цена печенья
+            double priceA = Convert.ToDouble(Console.ReadLine()); //цена яблок
+            Console.WriteLine(x9 * priceS + y9 * priceC + z9 * priceA);
+            */
+
+            /*
+            Console.WriteLine("#10");
+            Console.WriteLine("Мир Труд Май");
+            Console.WriteLine("Мир");
+            Console.WriteLine("     Труд");
+            Console.WriteLine("            Май");
+            */
+
+            //#11
+
+
+            //#12
+
+
+            /*
+            Console.WriteLine("#13");
+            Console.WriteLine("Введите любое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы ввели число" + a);
+            */
+
+            /*
+            Console.WriteLine("#14");
+            Console.WriteLine("2 кг");
+            Console.WriteLine("13  17");
+            */
+
+            /*
+            Console.WriteLine("#15");
+            int a15 = Convert.ToInt32(Console.ReadLine());
+            int b15 = Convert.ToInt32(Console.ReadLine());
+            int c15 = Convert.ToInt32(Console.ReadLine());
+            int d15 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a15);
+            Console.WriteLine(b15);
+            Console.WriteLine(c15);
+            Console.WriteLine(d15);
+            */
+
+            /*
+            Console.WriteLine("#16");
+            double a16 = Convert.ToDouble(Console.ReadLine());
+            double b16 = Convert.ToDouble(Console.ReadLine());
+            double c16 = Convert.ToDouble(Console.ReadLine());
+            double D = Math.Pow(b16,2) - 4 * a16 * c16;
+            if (D < 0)
+            {
+                Console.WriteLine("Нет корней");
+            }
+            else
+            {
+                double корень1 = (-b16 - Math.Sqrt(D)) / (2 * a16);
+                double корень2 = (-b16 + Math.Sqrt(D)) / (2 * a16);
+                Console.WriteLine(корень1 + " " + корень2);
+            }
+            */
+
+            /*
+            Console.WriteLine("#17");
+            double a17 = Convert.ToDouble(Console.ReadLine());
+            double b17 = Convert.ToDouble(Console.ReadLine());
+            double Sa = (a17 + b17) / 2;
+            double Sg = Math.Sqrt(a17 * b17);
+            Console.WriteLine("a) " + a17 + " b)" + b17);
+            */
+
+
+            /*
+            Console.WriteLine("#18");
+            double kx1 = Convert.ToDouble(Console.ReadLine());
+            double kx2 = Convert.ToDouble(Console.ReadLine());
+            double ky1 = Convert.ToDouble(Console.ReadLine());
+            double ky2 = Convert.ToDouble(Console.ReadLine());
+            double d18 = Math.Sqrt(Math.Pow(kx1-kx2,2) + Math.Pow(ky1-ky2,2));
+            Console.WriteLine(d18);
+            */
+
+            //#19
+
+
+            /*
+            Console.WriteLine("#20");
+            int n20 = Convert.ToInt32(Console.ReadLine());
+            int h = n20 / 60;
+            double min = ((double)h - d) * 60;
+            Console.WriteLine("a)" + h);
+            Console.WriteLine("b)" + min);
+            */
+
+            /*
+            Console.WriteLine("#21");
+            int площадьпр = 543 * 130;
+            double площадькв = Math.Pow(130, 2);
+            double n21 = площадьпр / площадькв;
+            Console.WriteLine((int)(n21));
+            */
+
+            /*
+            Console.WriteLine("#22");
+            int a22 = Convert.ToInt32(Console.ReadLine());
+            int b22 = a22 % 10;
+            int c22 = a22 / 10;
+            Console.WriteLine(b22 + "" + c22);
+            */
+
+            /*
+            Console.WriteLine("#28");
+            double a28 = Convert.ToDouble(Console.ReadLine());
+            double b28 = Convert.ToDouble(Console.ReadLine());
+            double c28 = Convert.ToDouble(Console.ReadLine());
+            double f28 = Math.Min(Math.Abs(a28), Math.Abs(b28));
+            double k28 = Math.Min(Math.Abs(c28), Math.Abs(f28));
+            Console.WriteLine(k28);
+            */
+
+            /*
+            Console.WriteLine("#29");
+            double a29 = Convert.ToDouble(Console.ReadLine());
+            double b29 = Convert.ToDouble(Console.ReadLine());
+            double c29 = Convert.ToDouble(Console.ReadLine());
+            double min1 = Math.Min(Math.Abs(a29), Math.Abs(b29));
+            double min2 = Math.Min(Math.Abs(c29), Math.Abs(min1));
+            double max1 = Math.Max(Math.Abs(a29), Math.Abs(b29));
+            double max2 = Math.Max(Math.Abs(c29), Math.Abs(max1));
+            Console.WriteLine(min2 + max2);
+            */
+
+            /*
+            Console.WriteLine("#34");
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("a)" + name);
+            Console.WriteLine("b)Привет, " + name + "!");
+            */
+        }
+    }
+}
